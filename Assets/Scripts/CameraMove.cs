f02095c704abeed2338a06d19fd72bc84f1c3721
Jaype27 <jaypee.0927@gmail.com>
2018-11-22 21:43:28 -0500

    player control, camera follow

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+	public PlayerController _playerControl;
+	private Vector3 _lastPos;
+	private float _distToMove;
+
+	// Use this for initialization
+	void Start () {
+		_playerControl = FindObjectOfType<PlayerController>();
+		_lastPos = _playerControl.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		_distToMove = _playerControl.transform.position.x - _lastPos.x;
+
+		transform.position = new Vector3(transform.position.x + _distToMove, transform.position.y, transform.position.z);
+
+		_lastPos = _playerControl.transform.position;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField]
+	private float _speed;
+	[SerializeField]
+	private float _jump;
+	[SerializeField]
+	private float _rayLength;
+	private Rigidbody _rb;
+	
+
+	// Use this for initialization
+	void Start () {
+		_rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_rb.velocity = new Vector2(_speed, _rb.velocity.y);
+		Debug.DrawRay(transform.position, -Vector3.up * _rayLength, Color.green);
+		Debug.Log(isGrounded());
+
+		if (isGrounded()) {
+			if(Input.GetKeyDown(KeyCode.Space)){
+				_rb.velocity = new Vector2(_rb.velocity.x, _jump);
+			}
+		}
+	}
+
+	bool isGrounded() {
+		return Physics.Raycast(transform.position, -Vector3.up, _rayLength);
+	}
+}
